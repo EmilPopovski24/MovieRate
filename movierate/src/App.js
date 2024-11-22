4c9ad5b2c492bbfd7403e7f31b2376b3318b0874
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-11-22 16:43:27 +0200

    create Home page and adding it to the Router in App.js

--- movierate/src/App.js
+++ movierate/src/App.js
@@ -1,12 +1,19 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
+import { Home } from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
         <Header />
+        <div className='main-content'>
+            <Routes >    
+                <Route path='/home' element={<Home />}/>
+            </Routes>
+        </div>
         <Footer />
     </div>
   );
