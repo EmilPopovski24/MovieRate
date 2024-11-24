2c9ab61edacef4ae6fd805129452639f5127f2dc
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-11-24 15:17:22 +0200

    update Login page css

--- movierate/src/components/Login/Login.js
+++ movierate/src/components/Login/Login.js
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom';
 
+import './Login.css';
+
 export const Login = () => {
     return (
         <>
            <section id="login-page" className="auth">
-            <form id="login" method="POST" style={{width:"50%", margin: "80px auto"}} >
+            <form className="login-form" method="POST">
                 <h1 className="login-header">Login</h1>
                 <div className="mb-3">
                     <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
