1ee89799b85fa4c258d03a9e7b9cfe7dfdee3a98
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-11-30 14:02:03 +0200

    adding react hooks and setup of onLoginSubmit

--- movierate/src/contexts/AuthContext.js
+++ movierate/src/contexts/AuthContext.js
@@ -1,4 +1,7 @@
 import { createContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { useLocalStorage } from "../hooks/useLocalStorage"; 
+import { authServiceFactory } from "../services/authService";
 
 
 export const AuthContext = createContext();
@@ -7,5 +10,27 @@ export const AuthProvider = ({
     children
 }) => {
 
+    const navigate = useNavigate();
+    const [auth, setAuth] = useLocalStorage('auth',{});
+    const authService = authServiceFactory(auth.accessToken)
+
+    const onLoginSubmit = async(data) => {
+        const result = await authService.login(data);
+        setAuth(result);
+        navigate('/catalog');
+        alert("You are welcome!")
+    }
+
+    const contextValues = {
+        onLoginSubmit,
+        token:auth.accessToken,
+        
+    }
+
+    return (
+        <AuthContext.Provider value={contextValues}>
+            {children}
+        </AuthContext.Provider>
+    )
 
 }
