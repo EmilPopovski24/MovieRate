620b68aebfe92e5e1902fae8612cc60e1bdcc1e0
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-11-30 15:55:28 +0200

    Update App.js

--- movierate/src/App.js
+++ movierate/src/App.js
@@ -1,6 +1,4 @@
-
 import { Route, Routes } from 'react-router-dom';
-
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 import { Home } from './components/Home/Home';
