1b6e8bb2ca1f2d103ddb01e325ba9fa76870eaa3
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-12-01 17:39:57 +0200

    Update MovieDetails.js

--- movierate/src/components/MovieDetails/MovieDetails.js
+++ movierate/src/components/MovieDetails/MovieDetails.js
@@ -3,7 +3,25 @@
 export const MovieDetails = () => {
     return (
         <>
-        <p>Movie Details page</p>
+         <section className="main">
+            <section className="movie-section">   
+                <div className="movie-img" >
+                     
+                </div>      
+                <div className="info">
+                    <div className="movie-title">
+                        
+                    </div>
+                    <div className="movie-info">
+                        <ul className="movie-info-ul">
+                            <li className="movie-info-li"><b>Director: </b></li>
+                            <li className="movie-info-li"><b>Genre: </b></li>
+                            <li className="movie-info-summary"><p id="text"></p></li>
+                        </ul>
+                    </div>
+                    </div>
+                </section>
+            </section>
         </>
     )
 }
