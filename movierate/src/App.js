55605c07698c8d05924d62f234234e72dfd242ed
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-12-06 17:47:09 +0200

    create react app and adding react router dom

---
+++ movierate/src/App.js
@@ -0,0 +1,20 @@
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
+import { Home } from './components/Home/Home';
+import { Route, Routes } from 'react-router-dom';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+		<Header />
+      		<Routes>
+    			<Route path="/" element={<Home />} />
+      		</Routes>
+		<Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ movierate/src/components/Header/Header.js
@@ -0,0 +1,9 @@
+
+
+export const Header = () => {
+    return (
+        <>
+        <p>Header</p>
+        </>
+    )
+}
