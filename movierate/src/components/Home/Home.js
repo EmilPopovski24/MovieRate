6ca50fcd3d81939f0481017e659cbb3820b965e2
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-12-06 18:29:34 +0200

    adding Home page to App router

--- movierate/src/components/Home/Home.js
+++ movierate/src/components/Home/Home.js
@@ -1,9 +1,17 @@
-
+import './Home.css'
 
 export const Home = () => {
     return (
         <>
-        <p>Home page</p>
+        <section className="homepage">
+            <div className="welcome-page">
+                <h1 className="welcome-message">Welcome to MovieRate</h1>
+                <h5 className="welcome-message">The unofficial Movie social media</h5>
+            </div>
+            <div className="cinema">
+                <img src="https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2luZW1hfGVufDB8fDB8fA%3D%3D&w=1000&q=80" alt="cinema" className="start-pic"/>
+            </div>
+        </section>
         </>
     )
 }
