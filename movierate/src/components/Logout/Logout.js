bd472019c0e24a644ee64e47ae3ff18f5e47bdc7
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-12-07 14:40:50 +0200

    Update Logout.js

--- movierate/src/components/Logout/Logout.js
+++ movierate/src/components/Logout/Logout.js
@@ -1,13 +1,11 @@
 import { useContext } from "react"
 import { AuthContext } from "../../contexts/AuthContext"
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 
 export const Logout = () => {
 
     const { onLogout } = useContext(AuthContext);
-    const navigate = useNavigate()
     onLogout()
-    navigate("/login")
-    return 
+    return <Navigate to='/login' />
 }
