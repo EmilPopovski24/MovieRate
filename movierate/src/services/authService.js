896190e2b68ccc22c55e2087e6c733b5c4c5557a
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-12-07 13:23:21 +0200

    Update authService.js

---
+++ movierate/src/services/authService.js
@@ -0,0 +1,14 @@
+import { requestFactory } from "./requester";
+
+const usersUrl = 'http://localhost:3030/users';
+
+export const authServiceFactory = (token) => {
+
+    const request = requestFactory(token);
+
+    return {
+        login: (loginData) => request.post(`${usersUrl}/login`, loginData),
+        register: (registerData) => request.post(`${usersUrl}/register`, registerData),
+        logout: () => request.get(`${usersUrl}/logout`)
+    }
+}
