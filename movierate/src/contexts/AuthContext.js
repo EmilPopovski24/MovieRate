2ec0c755963bb5de0d75181af3627dbf561eb2ab
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-12-07 13:25:30 +0200

    Update AuthContext.js

--- movierate/src/contexts/AuthContext.js
+++ movierate/src/contexts/AuthContext.js
@@ -1,5 +1,6 @@
 import { createContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { authServiceFactory } from "../services/authService";
 
 
 export const AuthContext = createContext();
@@ -8,8 +9,11 @@ export const AuthProvider = () => {
 
     const navigate = useNavigate();
     const [auth, setAuth] = useState('auth', {});
+    const authService = authServiceFactory(auth.accessToken);
 
     const onLoginSubmit = async(loginData) => {
+        const result = await authService.login(loginData);
+        setAuth(result);
         navigate('/catalog')
         return
     }
