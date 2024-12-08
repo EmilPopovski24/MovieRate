099ff3ee03649b6f84dcb97793468fbb6bf017e0
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-12-08 17:06:50 +0200

    Update MovieDetails.js

--- movierate/src/components/MovieDetails/MovieDetails.js
+++ movierate/src/components/MovieDetails/MovieDetails.js
@@ -1,9 +1,33 @@
+import { useState } from "react"
 
 
 export const MovieDetails = () => {
+
+    const [movie, setMovie] = useState({});
+
+    
+
     return(
         <>
-        <p>Movie Details</p>
+        <section className="main">
+            <section className="movie-section">   
+                <div>
+                    <img className="movie-img" src={movie.coverUrl} alt={movie.title} />
+                </div>      
+                <div className="info">
+                    <div className="movie-title">
+                        <h4>{movie.title} ({movie.year})</h4>
+                    </div>
+                    <div className="movie-info">
+                        <ul className="movie-info-ul">
+                            <li className="movie-info-li"><b>Director: </b>{movie.director}</li>
+                            <li className="movie-info-li"><b>Genre: </b>{movie.genre}</li>
+                            <li className="movie-info-summary"><p id="text">{movie.summary}</p></li>
+                        </ul>
+                    </div>
+                </div>
+            </section> 
+        </section> 
         </>
     )
 }
