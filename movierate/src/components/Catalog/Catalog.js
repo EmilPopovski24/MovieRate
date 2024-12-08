3afa4ec6d4f7cdeb19301aac29790695e5e02260
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-12-08 17:20:06 +0200

    Catalog and MovieItem css update

--- movierate/src/components/Catalog/Catalog.js
+++ movierate/src/components/Catalog/Catalog.js
@@ -3,6 +3,8 @@ import { useContext } from "react";
 import { MovieContext } from "../../contexts/MovieContext";
 import { MovieItem } from "./MovieItem/MovieItem";
 
+import "./Catalog.css";
+
 
 export const Catalog = () => {
 
@@ -11,13 +13,13 @@ export const Catalog = () => {
     return (
         <>
         {movies.length > 0 && (
-            <h1 className="catalog-movies">Pets</h1>
+            <h1 className="catalog-movies">Movies</h1>
         )}
         <div id='catalog-page'>
             {movies.map(x=> <MovieItem key={x._id} {...x} />)}
         </div>
         {movies.length === 0 && (
-            <h1 className="catalog-movies">No pet accounts yet</h1>
+            <h1 className="catalog-movies">No movies for review</h1>
         )}
         </>
     )
