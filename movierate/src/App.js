25acbee6c92bfc08ea546685637ddd6d6915501e
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-12-08 17:00:57 +0200

    Update App.js

--- movierate/src/App.js
+++ movierate/src/App.js
@@ -14,6 +14,7 @@ import { AuthProvider } from './contexts/AuthContext';
 
 import './App.css';
 import { MovieProvider } from './contexts/MovieContext';
+import { MovieItem } from './components/Catalog/MovieItem/MovieItem';
 // import { withAuth } from './components/hoc/withAuth';
 
 // const EnhancedLogin = withAuth(Login);
@@ -30,6 +31,7 @@ function App() {
 				<Route path="/register" element={<Register />} />
 				<Route path="/logout" element={<Logout />} />
 				<Route path="/catalog" element={<Catalog />} />
+				<Route path='/catalog/:movieId' element={<MovieItem />} />
 				<Route path='/addmovie' element={<AddMovie />} />
 				<Route path="/editmovie" element={<EditMovie />} />
 				<Route path='/account' element={<Account />} />
