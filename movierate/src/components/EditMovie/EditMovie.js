b0fee0e22b370ff8ccfbb04bfce11962c55ebb8a
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-12-11 21:19:52 +0200

    Update EditMovie.js

--- movierate/src/components/EditMovie/EditMovie.js
+++ movierate/src/components/EditMovie/EditMovie.js
@@ -26,22 +26,22 @@ export const EditMovie = () => {
                 <h3 className="edit-header">Edit Movie Details</h3>
 
                 <label htmlFor="title" className="form-label">Movie Title:</label>
-                <input  type="addmoviename" name ="title" className="form-control" id="title" aria-describedby="emailHelp" />
+                <input  type="addmoviename" name ="title" className="form-control" id="title" aria-describedby="emailHelp" value={values.title} onChange={changeHandler} />
             
                 <label htmlFor="year" className="form-label">Year:</label>
-                <input  type="year" name="year" className="form-control" id="year" aria-describedby="emailHelp" />
+                <input  type="year" name="year" className="form-control" id="year" aria-describedby="emailHelp" value={values.year} onChange={changeHandler} />
             
                 <label htmlFor="genre" className="form-label">Genre:</label>
-                <input  type="genre" name ="genre" className="form-control" id="genre" aria-describedby="emailHelp" />
+                <input  type="genre" name ="genre" className="form-control" id="genre" aria-describedby="emailHelp" value={values.genre} onChange={changeHandler}/>
             
                 <label htmlFor="director" className="form-label">Director:</label>
-                <input  type="director" name="director" className="form-control" id="director" aria-describedby="emailHelp" />
+                <input  type="director" name="director" className="form-control" id="director" aria-describedby="emailHelp" value={values.director} onChange={changeHandler}/>
             
                 <label htmlFor="coverUrl" className="form-label">Cover URL:</label>
-                <input  type="coverUrl" name="coverUrl" className="form-control" id="coverUrl" aria-describedby="emailHelp" />
+                <input  type="coverUrl" name="coverUrl" className="form-control" id="coverUrl" aria-describedby="emailHelp" value={values.coverUrl} onChange={changeHandler} />
             
                 <label htmlFor="summary">Summary:</label>
-                <textarea  name="summary" id="summary" className="form-control"></textarea>
+                <textarea  name="summary" id="summary" className="form-control" value={values.summary} onChange={changeHandler}></textarea>
                 
                 <button type="submit" className="btn-primary">Confirm changes</button>
 
