e406d3359c8a2a7b5dd7a55495f5cfcf1d61e50c
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-12-24 12:48:10 +0200

    AddComment css update

--- movierate/src/components/MovieDetails/AddComent/AddComment.js
+++ movierate/src/components/MovieDetails/AddComent/AddComment.js
@@ -6,7 +6,7 @@ export const AddComment = () => {
             <h4>Add your comment:</h4>
                 <form className="form">
                     <textarea name ="comment" className="comment-area" placeholder="Your comment..." ></textarea>
-                    <input className ="submit-btn"  type="submit" value="Publish" />
+                    <input className ="submit-comment-btn"  type="submit" value="Publish" />
                 </form>                       
         </article>
 
