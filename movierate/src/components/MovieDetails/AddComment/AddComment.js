08c99ad2119ef4119aeeb841a4cd4936ff9d7224
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-12-26 14:54:10 +0200

    Update AddComment.js

--- movierate/src/components/MovieDetails/AddComment/AddComment.js
+++ movierate/src/components/MovieDetails/AddComment/AddComment.js
@@ -9,15 +9,14 @@ export const AddComment = ({
     onCommentSubmit
 }) => {
 
-    const { username } = useContext(AuthContext);
+    // const { username } = useContext(AuthContext);
     const { values, changeHandler, onSubmit } = useForm({
-        username,
         comment:'',
     }, onCommentSubmit)
 
     return(
         <article className="addcomment">
-            <h4> {username} Add your comment:</h4>
+            <h4>Add your comment:</h4>
                 <form className="form" onSubmit={onSubmit}>
                     <textarea name ="comment" className="comment-area" placeholder="Your comment..." value={values.comment} onChange={changeHandler}></textarea>
                     <input className ="submit-comment-btn"  type="submit" value="Publish" />
