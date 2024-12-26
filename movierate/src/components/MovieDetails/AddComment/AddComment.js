0c6ca1c892ebdf01cd7ed133409e076ecfb90363
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-12-26 14:33:43 +0200

    Update AddComment.js

--- movierate/src/components/MovieDetails/AddComment/AddComment.js
+++ movierate/src/components/MovieDetails/AddComment/AddComment.js
@@ -1,21 +1,23 @@
+import { useContext } from 'react';
 import { useForm } from '../../../hooks/useForm';
 
 import './AddComment.css';
+import { AuthContext } from '../../../contexts/AuthContext';
 
 export const AddComment = ({
-    onCommentSubmit,
-   username
+    onCommentSubmit
 }) => {
 
+    const { username } = useContext(AuthContext);
     const { values, changeHandler, onSubmit } = useForm({
-        username,
         comment:'',
     }, onCommentSubmit)
 
     return(
         <article className="addcomment">
-            <h4>Add your comment:</h4>
+            <h4> {username} Add your comment:</h4>
                 <form className="form" onSubmit={onSubmit}>
+                   
                     <textarea name ="comment" className="comment-area" placeholder="Your comment..." value={values.comment} onChange={changeHandler}></textarea>
                     <input className ="submit-comment-btn"  type="submit" value="Publish" />
                 </form>                       
