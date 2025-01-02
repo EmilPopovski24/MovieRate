6d176925eaab47fb7f8004394d6b57a0c36292a4
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2025-01-02 14:33:55 +0200

    Update MovieDetails.js

--- movierate/src/components/MovieDetails/MovieDetails.js
+++ movierate/src/components/MovieDetails/MovieDetails.js
@@ -125,7 +125,7 @@ export const MovieDetails = () => {
                             <h5>Comments:</h5>                   
                                 {movie.comments && Object.values(movie.comments).map(x => (                                  
                                     <li key={x._id} className="comment-li">
-                                        <p className="comment-text"><b>{x.author.email}: </b>{x.comment}</p>
+                                        <p className="comment-text"><b>{x.author.email.split("@")[0]}: </b>{x.comment}</p>
                                     </li>
                                 ))}
                             </ul>  
